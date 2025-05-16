59b750ee579d6b000d9d2e4899aefe2eea8164ea
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-16 14:05:30 +0300

    enum

--- gamejam/scenes/block.gd
+++ gamejam/scenes/block.gd
@@ -1,6 +1,15 @@
 extends Node2D
-@export var visual = load("res://Images/GrassFloor.png")
+
+enum blockType{
+	Grass,
+	Wood,
+}
+
+@export var visual: blockType
 @onready var visualNode = $Sprite2D
 
 func _ready() -> void:
-	visualNode.texture = visual
+	if visual == blockType.Grass:
+		visualNode.texture = load("res://Images/GrassFloor.png")
+	elif visual == blockType.Wood:
+		visualNode.texture = load("res://Images/Wall.png")
