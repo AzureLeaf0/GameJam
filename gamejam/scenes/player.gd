b5bc60bec7dfa302e774b05d32ffe96a7946569f
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-16 18:03:46 +0300

    ikişeyler

--- gamejam/scenes/player.gd
+++ gamejam/scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 2000.0
+const JUMP_VELOCITY = -4000.0
 var coin = 0
 var Inventory = [null,null,null,null,null]
 var InventoryCapacity = 5
@@ -18,7 +18,7 @@ var touch_enemy = false
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity()*10 * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
