1976204d5f40bf7670284add7b92606bb06aefda
Koray Aydın <korayaydinn8@gmail.com>
2025-05-16 15:02:57 +0300

    enemy flip_h
    e

--- gamejam/scenes/enemy.gd
+++ gamejam/scenes/enemy.gd
@@ -4,10 +4,19 @@ var SPEED = 60
 
 var direction = 1
 
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+
+@onready var sprite: Sprite2D = $Sprite2D
+
 func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+		sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1	
+		sprite.flip_h = false
+		
 	position.x += direction * SPEED * delta
-	
-
-
 
 	
