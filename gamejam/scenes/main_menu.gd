b8397e24bf7fdef701535533ea69848f30950000
Koray Aydın <korayaydinn8@gmail.com>
2025-05-17 20:36:52 +0300

    credits menu

--- gamejam/scenes/main_menu.gd
+++ gamejam/scenes/main_menu.gd
@@ -21,7 +21,7 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_credits_pressed():
-	print("Credits açılacak (henüz yapılmadı)") 
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 	
 func _on_settings_pressed():
 	$OptionMenu.visible = !$OptionMenu.visible
