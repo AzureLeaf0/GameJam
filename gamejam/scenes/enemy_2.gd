318519fc3a24d03d1b80a71bc6d3c741ce746b86
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-17 09:37:03 +0300

    ui rework

--- gamejam/scenes/enemy_2.gd
+++ gamejam/scenes/enemy_2.gd
@@ -1,19 +1,40 @@
 extends RigidBody2D
 
 var SPEED = 1800
-var direction = 1
+var direction = 0
 
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
+func _ready():
+	sprite_2d.play("default")
+
 func _process(delta: float) -> void:
 	if direction == 0:
-		position.x+=1*delta
+		position.x+=10
+		sprite_2d.flip_h = false
 	elif direction == 1:
-		position.y+=1*delta
+		position.x+=6
+		position.y+=6
+		sprite_2d.flip_h = false
 	elif direction == 2:
-		position.x-=1*delta
+		position.y+=10
 	elif direction == 3:
-		position.y-=1*delta
+		position.x-=6
+		position.y+=6
+		sprite_2d.flip_h = true
+	elif direction == 4:
+		position.x-=10
+		sprite_2d.flip_h = true
+	elif direction == 5:
+		position.x-=6
+		position.y-=6
+		sprite_2d.flip_h = true
+	elif direction == 6:
+		position.y-=10
+	elif direction == 7:
+		position.x+=6
+		position.y-=6
+		sprite_2d.flip_h = false
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Sword"):
@@ -21,5 +42,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	randomize()
-	direction = randi_range(0,3)
+	direction = randi_range(0,7)
