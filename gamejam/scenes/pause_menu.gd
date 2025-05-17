147217d95962897c31a1b26c6417b4db9a9d88f8
Koray Aydın <korayaydinn8@gmail.com>
2025-05-17 22:04:45 +0300

    improved menus

--- gamejam/scenes/pause_menu.gd
+++ gamejam/scenes/pause_menu.gd
@@ -3,6 +3,12 @@ extends Control
 var _is_paused:bool = false:
 	set = set_paused
 
+@onready var option_menu: Control = $OptionMenu
+@onready var settings_button: TextureButton = $SettingsButton
+
+func _ready():
+	settings_button.pressed.connect(_on_settings_pressed)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		self._is_paused = !_is_paused
@@ -17,10 +23,17 @@ func _on_resume_pressed() -> void:
 	set_paused(false)
 
 func _on_restart_pressed() -> void:
-	pass
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 func _on_options_pressed() -> void:
-	pass #eklencek
+	$OptionMenu.visible = !$OptionMenu.visible
+	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed():
+	$OptionMenu.visible = !$OptionMenu.visible
+	
