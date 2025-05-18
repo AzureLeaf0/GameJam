3a196c069af5a0fea5fdd073ffc918c188c2612f
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-18 03:10:14 +0300

    aaaaaaaaaaaaaaaaaaaaaa

--- gamejam/scenes/enemy.gd
+++ gamejam/scenes/enemy.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var SPEED = 1800
 var direction = 1
+var touchedplayer
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
@@ -27,13 +28,22 @@ func _process(delta: float) -> void:
 	position.x += direction * SPEED * delta
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		touchedplayer = area
 	if area.is_in_group("Sword"):
-			$Area2D.remove_from_group("Enemy")
-			sprite.play("Die")
-			SPEED = 0
-			$DeathTimer.start()
-			$GPUParticles2D.emitting = true
+		if touchedplayer != null:
+			touchedplayer.get_parent().touch_enemy = false
+		$Area2D.remove_from_group("Enemy")
+		sprite.play("Die")
+		SPEED = 0
+		$DeathTimer.start()
+		$GPUParticles2D.emitting = true
 			
 
 func _on_death_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		touchedplayer = null
