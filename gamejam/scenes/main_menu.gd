002f3b1de04053ee79c09c54a4bfb910c11caabd
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-18 10:43:40 +0300

    animated

--- gamejam/scenes/main_menu.gd
+++ gamejam/scenes/main_menu.gd
@@ -15,7 +15,7 @@ func _ready():
 	settings_button.pressed.connect(_on_settings_pressed)
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/map.tscn")
+	get_tree().change_scene_to_file("res://scenes/AnimationScene.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
