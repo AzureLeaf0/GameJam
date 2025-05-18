f5fc881d07a75d939ba8086ea06d42bb6a0cd229
AzureLeaf0 <atakansezer05@hotmail.com>
2025-05-18 09:44:55 +0300

    aaaaaaaa

--- gamejam/scenes/ending_line.gd
+++ gamejam/scenes/ending_line.gd
@@ -4,6 +4,8 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
 		AtakanScoreSystem.Called(area.get_parent().death_count,area.get_parent().teleport_count,area.get_parent().UIElement.elapsed_time)
 		$CanvasLayer.visible = true
+		var minutes = AtakanScoreSystem.elapsed / 60
+		var seconds = AtakanScoreSystem.elapsed % 60
 		$CanvasLayer/GridContainer/Label.text = "Congrats! You have finished with %s points out of 3000 points!" %AtakanScoreSystem.score
 
 func _on_restart_pressed() -> void:
